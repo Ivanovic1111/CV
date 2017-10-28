457e6582ade2bd3ebf552ca3bff95b644bbf89ee
Svetlana <ivanovicsvetlana@yahoo.com>
2017-10-28 03:34:33 +0200

    Dodajem

---
+++ testing/kontakt_forma/lite_settings.php
@@ -0,0 +1,7 @@
+<?php
+
+$email_to = "ivanovicsvetlana@yahoo.com"; // your email address
+$email_subject = "Kontakt forma web kurs"; // email subject line
+$thankyou = "kontakt.html"; // thank you page
+
+?>
