f0fc69cb4b8d0a61487ea585fe24916f426e9625
Svetlana <ivanovicsvetlana@yahoo.com>
2018-01-06 11:43:33 +0100

    add cv

---
+++ CV-gh-pages/kontakt_forma/lite_settings.php
@@ -0,0 +1,7 @@
+<?php
+
+$email_to = "ivanovicsvetlana@yahoo.com"; // your email address
+$email_subject = "Kontakt forma web kurs"; // email subject line
+$thankyou = "hvala.html"; // thank you page
+
+?>
